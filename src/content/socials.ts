49f2d757429de5096ffed6c64f1b436025912bc0
Saideva98 <Saiharsha9826@gmail.com>
2025-12-02 22:41:50 -0500

    fix: Add missing 'id' field to Social objects

--- src/content/socials.ts
+++ src/content/socials.ts
@@ -4,6 +4,7 @@ export const socials: Social[] = [
   {
     _id: "social1",
     _type: "social",
+    id: "github",
     name: "GitHub",
     icon: "github",
     link: "https://github.com/Saideva9826",
@@ -11,6 +12,7 @@ export const socials: Social[] = [
   {
     _id: "social2",
     _type: "social",
+    id: "linkedin",
     name: "LinkedIn",
     icon: "linkedin",
     link: "https://www.linkedin.com/in/saideva-goudnathi/",
@@ -18,6 +20,7 @@ export const socials: Social[] = [
   {
     _id: "social3",
     _type: "social",
+    id: "email",
     name: "Email",
     icon: "email",
     link: "saideva.goudnathi@gmail.com",
